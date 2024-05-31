aeab2568663f5d1b3fcc76b6f1b2740e7a342b21
ipithani <pithaninarendra396@gmail.com>
2024-05-31 15:06:44 +0530

    data ingestion added

--- demo.py
+++ demo.py
@@ -1,10 +1,4 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-import sys
+from us_visa.pipline.training_pipeline import TrainPipeline
 
-# logging.info("welcome to our custom log")
-
-try:
-    a = 1/"10"
-except Exception as e:
-    raise USvisaException(e, sys) from e
+pipline  = TrainPipeline()
+pipline.run_pipeline()
